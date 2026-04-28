d8b1c642892f6203b512a3e2cb5617f936984597
clamlum2 <clamlum2@gmail.com>
2026-04-28 01:34:19 +0000

    init

---
+++ src/client/java/com/clamlum/fuzzycreativesearch/client/FuzzyCreativeSearchClient.java
@@ -0,0 +1,10 @@
+package com.clamlum.fuzzycreativesearch.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FuzzyCreativeSearchClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
